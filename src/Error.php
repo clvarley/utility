c0b3c0d36797c3eec7504c3e8e8b15d2d7c7b164
clvarley <conorlukevarley@gmail.com>
2025-12-19 16:01:08 +0000

    Provide suppression utility that returns result type

--- src/Error.php
+++ src/Error.php
@@ -2,10 +2,14 @@
 
 namespace Clvarley\Utility;
 
+use function assert;
+use function error_get_last;
 use function error_reporting;
 
 /**
  * @api
+ *
+ * @psalm-type Error = array{type: int, message: string, file: string, line: int}
  */
 final class Error
 {
@@ -27,5 +31,27 @@ final class Error
         }
 
         return $result;
+    }
+
+    /**
+     * @template T
+     *
+     * @param callable():T $callback
+     *
+     * @return array{0: T, 1: null}|array{0: T|null, 1: Error}
+     */
+    public static function suppressResult(callable $callback): array
+    {
+        $previousError = error_get_last();
+        $result = self::suppress($callback);
+        $currentError = error_get_last();
+
+        if ($currentError !== $previousError) {
+            assert(null !== $currentError);
+
+            return [$result, $currentError];
+        }
+
+        return [$result, null];
     }
 }
